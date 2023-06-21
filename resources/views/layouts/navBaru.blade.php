cb1b57821ad8d891af163319e4ba0f371931f52d
“hikmat <hidayathikmat8@gmail.com>
2023-06-21 20:54:31 +0700

    merubah navbar jadi rata kiri dan penambahan di navbar hki

--- resources/views/layouts/navBaru.blade.php
+++ resources/views/layouts/navBaru.blade.php
@@ -4,25 +4,28 @@
     @endguest
     @auth
         <li><a class="nav-link scrollto " href="{{ url('dashboard') }}">Beranda</a></li>
-
+        {{-- supllier --}}
         @if (Auth::user()->role_id == '3')
             <li><a class="nav-link scrollto" href="{{ route('supplier.po.index') }}">Purchase Order</a></li>
             <li><a class="nav-link scrollto" href="{{ route('supplier.surat.index') }}">Surat Jalan</a></li>
         @endif
-
+        {{-- subcon --}}
         @if (Auth::user()->role_id == '2')
             <li><a class="nav-link scrollto" href="{{ route('subcon.po.index') }}">Purchase Order</a></li>
             <li><a class="nav-link scrollto" href="{{ route('subcon.surat.index') }}">Surat Jalan</a></li>
         @endif
+        {{-- hki --}}
         @if (Auth::user()->role_id == '1')
-            <li><a class="nav-link scrollto" href="{{ route('hki.surat.index') }}">Surat Jalan</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('hki.user.index') }}">Data Master</a></li>
             <li class="dropdown"><a href="#"><span>Purchase Order</span> <i class="bi bi-chevron-down"></i></a>
                 <ul>
                     <li><a href="{{ route('hki.po.supplier.index') }}">Supplier</a></li>
                     <li><a href="{{ route('hki.po.subcon.index') }}">Subcon</a></li>
                 </ul>
             </li>
-            <li><a class="nav-link scrollto" href="{{ route('hki.user.index') }}">Manage User</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('hki.surat.index') }}">Surat Jalan</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('hki.surat.index') }}">Sisa Barang</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('hki.surat.index') }}">Monitor Surat</a></li>
         @endif
         <li class="dropdownx"><a class="getstarted scrollto" href="#">{{ Auth::user()->username }}<i
                     class="bi bi-chevron-down"></i></a>
